f43fb600632de10cc880018e5b3cf37990bf59e6
Sai-M021 <sai.manchikalapati@gmail.com>
2018-11-04 19:25:28 -0500

    Add files via upload

--- SalesmanGA_ex.py
+++ SalesmanGA_ex.py
@@ -5,11 +5,11 @@
 # - Amount of Reproducing parents
 # - Amount of Members in the Tournament (see selection function)
 # - Mutation Rate (randomely determined)
+# Note: Number of Cities needs to be considerably large (10 - 25)
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import random
-#Initial Population Size must be over 10 cities
 class City:
     def __init__(self, xVal, yVal):
         self.x = xVal
@@ -30,7 +30,7 @@ def f(route):
             distance = distance + (route[i]).distance(route[0])
     return 1 / distance
 
-def intial(cities, size):
+def initial(cities, size):
     population = list()
     for i in range(0, size):
         population.append(random.sample(cities, len(cities)))
@@ -50,11 +50,10 @@ def select(fit, size, k):
     k_list = list()
     while len(parents) < size:
         random.shuffle(individuals)
-        k_list = individuals[0:k + 1]
+        k_list = individuals[0 : k]
         parents.append(max(k_list)[1])
     return parents
 
-#swap mutation
 def mutate(population, rate):
     for i in population:
         if random.random() < rate:
@@ -64,22 +63,52 @@ def mutate(population, rate):
             i[0] = temp
     return population
 
-#TODO: breed the parents and return the population
-def breed(parents):
-    return
+def breed(parent1, parent2):
+    child = []
+    childP1 = []
+    childP2 = []
+
+    geneA = int(random.random() * len(parent1))
+    geneB = int(random.random() * len(parent1))
+
+    startGene = min(geneA, geneB)
+    endGene = max(geneA, geneB)
+
+    for i in range(startGene, endGene):
+        childP1.append(parent1[i])
+
+    childP2 = [item for item in parent2 if item not in childP1]
+
+    child = childP1 + childP2
+    return child
+
+def breedPopulation(parents, elite):
+    children = []
+    length = len(parents) - elite
+    pool = random.sample(parents, len(parents))
+
+    for i in range(0, elite):
+        children.append(parents[i])
+    for i in range(0, length):
+        children.append(breed(parents[i], parents[len(matingpool) - i - 1]))
+    return children
 
 def generation(current, elite, k, mutation):
     fit = fitness(current)
     parents = select(fit, elite, k)
-    children = breed(parents)
+    children = breedPopulation(parents, elite)
     next_gen = mutate(children, mutation)
     return next_gen
 
-#TODO: generate n amount of cities each with a random weight
+#bounds x: -100, 100 and y:-100, 100
 def generateCities(n):
-    return
+    cities = list()
+    for i in range(0, n):
+        cities.append(City(int(random.random() * 100), int(random.random() * 100)))
+    return cities
 
-def main(void):
+
+def main():
     count = int(input("Amount of Cities: "))
     elite = int(input("Amount of Parents: "))
     k = int(input("Tournament Member Count: "))
